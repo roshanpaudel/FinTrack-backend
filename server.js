0fe27db002597ff89da89a6f7be1bce67ed3a1ee
Roshan Paudel <paudelroshan93@gmail.com>
2025-08-03 19:50:36 +1000

    Express server created and rest.http created for HTTP testing

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Server Started");
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "We are Live",
+  });
+});
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`Server Running at http://localhost:${PORT}`);
+});
