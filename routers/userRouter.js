8e989a32362201870c818fa7b77c1e0bc002dea1
Roshan Paudel <paudelroshan93@gmail.com>
2025-08-04 20:56:06 +1000

    signup router and post method added

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,2 +1,20 @@
 import express from "express";
 const router = express.Router();
+
+// User signup
+router.post("/", (req, res) => {
+  try {
+    console.log(req.body);
+    res.json({
+      status: "success",
+      message: "TODO",
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+// User login
+// User profile
