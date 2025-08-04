994f3442b273b2eca0bff735721696d98f80bfdd
Roshan Paudel <paudelroshan93@gmail.com>
2025-08-04 21:05:12 +1000

    endpoint creaed for userRouter

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
 
+
 // User signup
 router.post("/", (req, res) => {
   try {
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+import userRouter from "./routers/userRouter.js";
+//Api endpoints
+app.use("api/v1/users", userRouter);
 app.get("/", (req, res) => {
   res.json({
     message: "We are Live",
