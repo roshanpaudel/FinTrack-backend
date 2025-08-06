8f164c7e6d8e40d56ce6709f0859c8ede146dd5f
Roshan Paudel <paudelroshan93@gmail.com>
2025-08-06 19:59:59 +1000

    hash function created to encrypt password

--- config/mongodbConfig.js
+++ config/mongodbConfig.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 const MONGO_URL = "mongodb://localhost:27017/financial_tracker";
 
-export const connectMongoDB = () => {
+export const connectMongoDB = async () => {
   try {
-    const conn = mongoose.connect(MONGO_URL);
+    const conn = await mongoose.connect(MONGO_URL);
     conn && console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,18 +1,21 @@
 import express from "express";
 import { insertUser } from "../models/user/userModel.js";
+import { hashPassword } from "../utils/bcryptJs.js";
 const router = express.Router();
 
 // User signup
 router.post("/", async (req, res) => {
   try {
-    //get the user object
     //encrypt the password
+    req.body.password = hashPassword(req.body.password);
+    console.log(req.body.password);
+
     //insert the user
     const user = await insertUser(req.body);
     user?._id
       ? res.json({
           status: "success",
-          message: "Your account has been creaed. You may login now",
+          message: "Your account has been created. You may login now",
         })
       : res.json({
           status: "error",
